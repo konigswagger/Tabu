cf33f9f11cb05e4e9df230f5d9fdbdfa00cc798b
Nancydragon12345 <nancydragon12345@gmail.com>
2011-11-06 16:35:20 -0800

    Just added my files and replaced the main and string xml files since they were just the default hello world messages.

---
+++ tabu/src/edu/berkeley/bid/cs160/opus/IntroChinaActivity.java
@@ -0,0 +1,13 @@
+package edu.berkeley.bid.cs160.opus;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class IntroChinaActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.introchina);
+	}
+}
