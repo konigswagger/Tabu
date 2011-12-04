9ebc622f12a91362ae63de81a93db3c7216418d4
Gautam Jain <gautamjain@gmail.com>
2011-12-03 19:09:27 -0800

    .

--- tabu/src/edu/berkeley/bid/cs160/opus/OpusActivity.java
+++ tabu/src/edu/berkeley/bid/cs160/opus/OpusActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 public class OpusActivity extends Activity {
 	public String user;
