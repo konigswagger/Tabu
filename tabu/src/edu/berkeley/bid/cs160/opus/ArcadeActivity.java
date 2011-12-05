ee5a98c189be713979db721a970aa03fc306704f
josefjohn <josefjohn88@gmail.com>
2011-12-04 22:05:47 -0800

    arcadeHelp

--- tabu/src/edu/berkeley/bid/cs160/opus/ArcadeActivity.java
+++ tabu/src/edu/berkeley/bid/cs160/opus/ArcadeActivity.java
@@ -1,9 +1,14 @@
 package edu.berkeley.bid.cs160.opus;
 
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -107,4 +112,29 @@ public class ArcadeActivity extends OpusActivity {
 	    toast.show();
 	}
 	
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+        case R.id.menu_help:
+        	// Do something when help is clicked.
+        	Builder builder = new AlertDialog.Builder(this);
+    		builder.setTitle("Help");
+    		builder.setMessage("'TABU' on the top left takes you back to the Main Menu.\n\n" + 
+    				"'?' on the top right takes you to this pop-up help screen.\n\n" +
+    				"Read the text and answer the questions by clicking on the blue buttons.\n\n" +
+    				"A right answer awards you positive points and a wrong answer gains you negative points." +
+    				"Completing the level opens up the next level.");
+    		builder.setCancelable(false);
+    		builder.setNegativeButton("Close", new OnClickListener() {
+    			public void onClick(DialogInterface dialog, int id) {
+    				dialog.cancel();
+    			}
+    		});
+    		AlertDialog help = builder.create();
+    		help.show();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
 }
